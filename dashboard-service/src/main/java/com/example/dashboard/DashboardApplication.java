5846088b4b4e90699f7d8a369e87923dfafdbe30
Manuel Páez <m.paez589@gmail.com>
2018-02-17 14:32:24 +0100

    Add files via upload

---
+++ dashboard-service/src/main/java/com/example/dashboard/DashboardApplication.java
@@ -0,0 +1,17 @@
+package com.example.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableEurekaClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class DashboardApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "Dashboard");
+		SpringApplication.run(DashboardApplication.class, args);
+	}
+}
